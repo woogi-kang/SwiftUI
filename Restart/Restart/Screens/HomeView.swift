e562653e6bd2c34d78e9ccadf3852d19ef3bb77a
강태욱 <hotsunny.kang@gmail.com>
2022-04-14 16:34:48 +0900

    Update: 공통 view 클래스 추가, HomeView디자인 추가

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -16,14 +16,45 @@ struct HomeView: View {
     
     var body: some View {
         VStack (spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            // MARK: - CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            
+            Spacer()
             
             Button(action: {
                 isOnbardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
     }
 }
