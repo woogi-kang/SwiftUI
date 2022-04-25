060e1e0e193069779eef925e06df0a2cf55f6e39
강태욱 <hotsunny.kang@gmail.com>
2022-04-25 17:21:16 +0900

    Update: Complex gridview 추가 toolbar에 버튼 추가

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -8,30 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      let animals: [Animal] = Bundle.main.decode("animals.json")
-      
-      NavigationView {
-        List {
-          CoverImageView()
-            .frame(height: 300)
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-          
-          ForEach(animals) { animal in
-            NavigationLink(destination: AnimalDetailView(animal: animal)) {
-              AnimalListItemView(animal: animal)
+  // MARK: - PROPERTIES
+  let animals: [Animal] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
+  
+  @State private var isGridViewActive: Bool = false
+  @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+  @State private var gridColumn: Int = 1
+  @State private var toolbarIcon: String = "square.grid.2x2"
+  
+  // MARK: - FUNCTIONS
+  
+  func gridSwitch() {
+    gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+    gridColumn = gridLayout.count
+    print("Grid Number: \(gridColumn)")
+    
+    // TOOLBAR IMAGE
+    switch gridColumn {
+    case 1:
+      toolbarIcon = "square.grid.2x2"
+    case 2:
+      toolbarIcon = "square.grid.3x2"
+    case 3:
+      toolbarIcon = "rectangle.grid.1x2"
+    default:
+      toolbarIcon = "square.grid.2x2"
+    }
+  }
+    
+  var body: some View {
+    NavigationView {
+      Group {
+        if !isGridViewActive {
+          List {
+            CoverImageView()
+              .frame(height: 300)
+              .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            
+            ForEach(animals) { animal in
+              NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                AnimalListItemView(animal: animal)
+              }
+            }
+          } // List
+        } else {
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+              ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                  AnimalGridItemView(animal: animal)
+                }
+              }
+            } // GRID
+            .padding(10)
+            .animation(Animation.easeInOut(duration: 1.0), value: isGridViewActive)
+          }
+        }
+      }
+      .listStyle(PlainListStyle())
+      .navigationTitle("Africa")
+      .navigationBarTitleDisplayMode(.large)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          HStack(spacing: 16) {
+            // LIST
+            Button(action: {
+              isGridViewActive = false
+              haptics.impactOccurred()
+            }) {
+              Image(systemName: "square.fill.text.grid.1x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+            }
+            
+            // GRID
+            Button(action: {
+              isGridViewActive = true
+              haptics.impactOccurred()
+              gridSwitch()
+            }) {
+              Image(systemName: toolbarIcon)
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .accentColor : .primary)
             }
           }
-        } // List
-        .listStyle(PlainListStyle())
-        .navigationTitle("Africa")
-        .navigationBarTitleDisplayMode(.large)
-      } // Navigation
-    }
+        }
+      }
+    } // Navigation
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
